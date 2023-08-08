6a53585bc8422487d9f540f85e2d54526b1f0167
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-08-08 10:17:56 -0300

    :tada:

--- scripts/route.js
+++ scripts/route.js
@@ -12,22 +12,27 @@ export class Routers {
 
         window.history.pushState({},"", event.target.href)
 
+        if(window.location.pathname == '/universo'){
+         document.body.style.backgroundImage = 'url(./imgs/mountains-universe-2.png)'
+        } else if(window.location.pathname == '/exploracao'){
+            document.body.style.backgroundImage = 'url(./imgs/mountains-universe-3.png)'
+        } else {
+            'url(./imgs/mountains-universe-1.png)'
+        }
+
+
         this.handle()    
     }
 
     handle(){
 
         const {pathname} = window.location
-        const route = this.routes[pathname] // || this.routes[404]
+        const route = this.routes[pathname] || this.routes[404]
         
         fetch(route).then(data => data.text()).then(html => {
             document.getElementById("#app").innerHTML = html
         })
         
         console.log(pathname)
-
-        // if(pathname === "/universo"){
-        //     document.documentElement.style.backgroundImage.
-        // }
     }
 }
