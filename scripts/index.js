316674eebd6c0aa7ee87b76312994ddd1eea56dc
Lkaua22k <lkauan462@gmail.com>
2023-08-09 22:35:59 -0300

    :tada:

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,10 @@ import { Routers } from "./route.js";
 
 const routers = new Routers()
 
-routers.add("/","./pages/home.html")
-routers.add("/universo","./pages/universe.html")
-routers.add("/exploracao","./pages/exploracao.html")
-// routers.add("/404","./pages/404.html")
+routers.add('/', './pages/home.html')
+routers.add('/universo', './pages/universe.html')
+routers.add('/exploracao', './pages/exploracao.html')
+routers.add('/404', './pages/404.html' )
 
 routers.handle()
 
